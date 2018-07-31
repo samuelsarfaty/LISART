30f200c88ca9806bb2e40c0ca82ab805c0224ffe
Samuel Sarfaty <samuel.sarfaty@gmail.com>
2018-07-31 15:10:32 +0100

    Built scene + started to work on game manager

---
+++ Assets/Scripts/SART_TestManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SART_TestManager : MonoBehaviour {
+
+	public int blockNumber;
+	public int goCount;
+	public int noGoCount;
+
+	public float stimulusWaitTime;
+	public float delayBetweenStimuli;
+	public float cummulativeTime;
+
+	public GameObject go;
+	public GameObject noGo;
+	public GameObject door;
+
+	void Start(){
+		Reset ();
+	}
+
+	void Update(){
+		
+	}
+
+	void ShowGo(){
+		door.SetActive (false);
+		go.SetActive (true);
+	}
+
+	void ShowNoGo(){
+		door.SetActive (false);
+		noGo.SetActive (false);
+	}
+
+	void Reset(){
+		go.SetActive (false);
+		noGo.SetActive (false);
+		door.SetActive (true);
+	}
+		
+}
