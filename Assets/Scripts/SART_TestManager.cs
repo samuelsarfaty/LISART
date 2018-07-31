5da4b8abc40c9d09761b407fbbfaff709d8c90ca
Samuel Sarfaty <samuel.sarfaty@gmail.com>
2018-07-31 17:25:27 +0100

    testing

--- Assets/Scripts/SART_TestManager.cs
+++ Assets/Scripts/SART_TestManager.cs
@@ -11,29 +11,62 @@ public class SART_TestManager : MonoBehaviour {
 	public float delayBetweenStimuli;
 	public float cummulativeTime;
 
+	private float lastActionTime;
+
 	public GameObject go;
 	public GameObject noGo;
 	public GameObject door;
+
+	private bool isWaiting;
+
+
+	void Awake(){
+		isWaiting = false;
+		lastActionTime = Time.time;
+	}
 
 	void Start(){
 		Reset ();
 	}
 
-	void Update(){
-		
+	void FixedUpdate(){
+		if(!isWaiting){
+			if(Time.time - lastActionTime >= delayBetweenStimuli){
+				lastActionTime = Time.time;
+				ShowGo ();
+			}
+		} else if (isWaiting){
+			if(Time.time - lastActionTime >= stimulusWaitTime){
+				lastActionTime = Time.time;
+				Reset ();
+			}
+		}
+
+		print (Time.time);
 	}
 
 	void ShowGo(){
+		print ("Shown!");
+
+		isWaiting = true;
 		door.SetActive (false);
+		noGo.SetActive (false);
 		go.SetActive (true);
+		goCount++;
 	}
 
 	void ShowNoGo(){
+		isWaiting = true;
 		door.SetActive (false);
-		noGo.SetActive (false);
+		go.SetActive (false);
+		noGo.SetActive (true);
+		noGoCount++;
 	}
 
 	void Reset(){
+		print ("Reset");
+
+		isWaiting = false;
 		go.SetActive (false);
 		noGo.SetActive (false);
 		door.SetActive (true);
